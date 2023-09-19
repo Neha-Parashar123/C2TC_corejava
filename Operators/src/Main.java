bd0cf231de995e872029e6b6622077c932e440bf
Neha Parashar <130543508+Neha-Parashar123@users.noreply.github.com>
2023-09-19 16:20:26 +0530

    First Commit

---
+++ Operators/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int x = 10;
+		System.out.println(a + "" +b);
+		int c = ++a +b +a--;
+		System.out.println(c);
+		System.out.println(a);
+		
+		int d = c++ +a + b--;
+		System.out.println(d);
+
+	}
+
+}
---
+++ switch_case/src/switch_case/Switch_case.java
@@ -0,0 +1,49 @@
+package switch_case;
+
+public class Switch_case {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day = 4;
+		switch (day) {
+		  case 1:
+		    System.out.println("Monday");
+		    break;
+		  case 2:
+		    System.out.println("Tuesday");
+		    break;
+		  case 3:
+		    System.out.println("Wednesday");
+		    break;
+		  case 4:
+		    System.out.println("Thursday");
+		    break;
+		  case 5:
+		    System.out.println("Friday");
+		    break;
+		  case 6:
+		    System.out.println("Saturday");
+		    break;
+		  case 7:
+		    System.out.println("Sunday");
+		    break;
+		}
+		
+		//Break Statement
+		int day = 4;
+		switch (day) {
+		  case 6:
+		    System.out.println("Today is Saturday");
+		    break;
+		  case 7:
+		    System.out.println("Today is Sunday");
+		    break;
+		  default:
+		    System.out.println("Looking forward to the Weekend");
+		}
+
+
+
+	}
+
+}
