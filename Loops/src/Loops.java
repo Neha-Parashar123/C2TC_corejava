e7bd7742a62c6acd40b929e29b62b5d72e1c6d18
Neha Parashar <130543508+Neha-Parashar123@users.noreply.github.com>
2023-09-19 16:24:19 +0530

    First Commit

---
+++ Loops/src/Loops.java
@@ -0,0 +1,24 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		 for(int i=1;i<=10;i++){  
+		        System.out.println(i);  
+		    } 
+		 //while loop
+		 int i = 0;
+		 while (i < 5) {
+		   System.out.println(i);
+		   i++;
+		 }
+		 //for each loop
+		 int a[] = {10,20,30,40,50};
+		 for(int i : a) {
+			 System.out.println(i);
+		 }
+		 
+		// TODO Auto-generated method stub
+
+	}
+
+}
